22cd00cb6b7fca0ceba1667e30faecef5d193cc2
CheshtaK <32400008+CheshtaK@users.noreply.github.com>
2018-02-08 18:45:28 +0530

    adding device token and sending notification

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ admin.initializeApp(functions.config().firebase);
 exports.sendNotification = functions.database.ref('/notifications/{user_id}/{notification_id}').onWrite(event => {
 
   const user_id = event.params.user_id;
-  const notification_id = event.params.notification_id; 
+  const notification_id = event.params.notification_id;
 
   console.log('We have a notification from : ', user_id);
 
@@ -17,18 +17,22 @@ exports.sendNotification = functions.database.ref('/notifications/{user_id}/{not
   return console.log('A Notification has been deleted from the database : ', notification_id);
 
 }
+   const deviceToken = admin.database().ref(`/Users/${user_id}/device_token`).once('value');
 
-const payload = {
-      notification: {
-        title : "New Friend Request",
-        body: `Your friend has sent you request`,
-        icon: "default",
-      };
+   return deviceToken.then(result => {
 
-return admin.messaging().sendToDevice(token_id, payload).then(response => {
+     const payload = {
+       notification: {
+         title : "New Friend Request",
+         body: `Friend has sent you request`,
+         icon: "default",
+       };
 
-              console.log('This was the notification Feature');
+     return admin.messaging().sendToDevice(token_id, payload).then(response => {
 
-            });
+       console.log('This was the notification Feature');
 
+   });
+
+ });
 });
